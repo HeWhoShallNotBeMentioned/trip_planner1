b2f04cc631be15901fb084f3f28470e9991ece5e
Brett Scheller <brett.green.scheller@gmail.com>
2018-09-25 15:14:25 -0500

    about to get api key

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Can you see me?");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "YOUR API TOKEN HERE";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885],
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
